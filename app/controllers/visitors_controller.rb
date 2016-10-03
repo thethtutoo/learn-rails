19e9128a82a037221afae07eaeff0317a5eabd43
Thet Htut Oo <tho2410@gmail.com>
2016-10-03 12:50:12 +0630

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,6 @@
 class VisitorsController < ApplicationController
     def new
         @owner = Owner.new
-        flash[:notice] = 'Welcome!'
-        flash[:alert] = 'My birthday is soon.'
+        
     end
 end
